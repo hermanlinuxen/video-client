c1195009c45ec87a3919277126e1dbe3d432ff9d
Herman <hermanlinuxen@protonmail.com>
2024-02-06 00:13:08 +0100

    changes

--- main.cpp
+++ main.cpp
@@ -16,11 +16,16 @@ https://en.cppreference.com/w/cpp/language/ascii
 //using namespace std;
 
 // Global Variables
-bool logging = true;
+bool logging = false;
 bool logfile = false;
 bool collapse_threads = false;
 
-unsigned char input_character; // Input character.
+// input validation variables
+int input_character;
+bool input_character_exit = false;
+bool exit_thread_started = false;
+bool typing_mode = false;
+unsigned char char_input_character;
 
 // colors:
 const std::string color_reset  = "\033[0m";
@@ -71,52 +76,87 @@ void log(std::string message, int severity = 0) {
     }
 }
 
+// Exit check thread, multiple keys return 27 including escape and arrow keys.
+void THREAD_input_verify_exit () {
+    usleep(150); // 0.15MS
+    if ( input_character == 27 ) {
+        log("Received exit signal.");
+        collapse_threads = true;
+    } else {
+        if ( input_character == 91 ) {
+            usleep(500); // 0.5MS
+            if ( input_character == 65 ) {
+                log("Arrow Up");
+            } else if ( input_character == 66 ) {
+                log("Arrow Down");
+            } else if ( input_character == 68 ) {
+                log("Arrow Left");
+            } else if ( input_character == 67 ) {
+                log("Arrow Right");
+            } else {
+                log("Unknown key!", 2);
+            }
+        }
+    }
+    if ( collapse_threads == true ) {
+        std::cout << "Press any key to exit...\n";
+    }
+}
+
+//
+void determine_input ( int input = 0 ) {
+    if ( input == 0 ) {
+        return;
+    }
+    log("Determining input for: " + std::to_string(input));
+    if ( input == 27 ) {
+        std::thread input_thread_verify_exit(THREAD_input_verify_exit);
+        input_thread_verify_exit.detach();
+    }
+}
+
+// Input thread keeping track of inputs, and adding to queue.
 void THREAD_input () {
+    log("Started input thread.");
     struct termios old_tattr, new_tattr;
     tcgetattr(STDIN_FILENO,&old_tattr);
     new_tattr=old_tattr;
     new_tattr.c_lflag &=(~ICANON & ~ECHO);
     tcsetattr(STDIN_FILENO,TCSANOW,&new_tattr);
     // Continous loop
-    while ( input_character != 'q') {
-        input_character=getchar();
+    while ( true ) {
+        usleep(100); // 0.1MS
         if ( collapse_threads == true ) {
             break;
         }
-        //std::cout << "Input: " << input_character << "\n";
-        printf("%d ", input_character);
+        if ( typing_mode == true ) {
+            char_input_character=getchar();
+            input_character = (int)char_input_character;
+        } else {
+            input_character=getchar();
+        }
+        determine_input(input_character);
     }
     tcsetattr(STDIN_FILENO,TCSANOW,&old_tattr);
 }
 
 int main() {
 
-    log("Very long and complicated log message!!!");
-    log("Warning message!", 1);
-    log("Errormessage!", 2);
-    log("FATALITY!!!", 3);
-
-    std::string printmessage;
-
-    printmessage = color_red + "message is red" + color_reset + "\n";
-    std::cout << printmessage;
-    printmessage = color_blue + "message is blue" + color_reset + "\n";
-    std::cout << printmessage;
-    printmessage = color_cyan + "message is cyan" + color_reset + "\n";
-    std::cout << printmessage;
-    printmessage = color_green + "message is green" + color_reset + "\n";
-    std::cout << printmessage;
-    printmessage = color_yellow + "message yellow" + color_reset + "\n";
-    std::cout << printmessage;
-
     std::thread input_thread(THREAD_input);
 
-    log("Sleep start");
-    usleep(10000000);
-    log("Sleep stop");
+    while ( true ) { // Main loop
+
+        usleep(10000); // 10MS
+        if ( collapse_threads == true ) {
+            log("Exiting main function");
+            break;
+        }
+    }
+
     collapse_threads = true;
-    
+
     input_thread.join();
+    usleep(1000);
 
     return 0;
 }
