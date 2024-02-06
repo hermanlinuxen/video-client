78dff9eb8bad8e8c47a30ac630f6a331be9406db
Herman <hermanlinuxen@protonmail.com>
2024-02-06 21:07:47 +0100

    Add logging to file, WIP ui backend and WIP argument parsing.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,29 @@
 // Video Client Main
 
-/*
-https://en.cppreference.com/w/cpp/language/ascii
-
+/* 
+Resources
+    - https://en.cppreference.com/w/cpp/language/ascii
 */
 
 // Libraries
 #include <iostream>
+#include <sys/ioctl.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <termios.h>
 #include <thread>
-
-// Namespace
-//using namespace std;
+#include <fstream>
 
 // Global Variables
-bool logging = false;
-bool logfile = false;
+bool debug = false;
+bool log_to_file = true;
 bool collapse_threads = false;
+
+// Global Constants
+const std::string homedir = getenv("HOME");
+const std::string userconfigdir = homedir + "/.config";
+const std::string configdir = userconfigdir + "/video-client";
+const std::string logfile = configdir + "/video-client.log";
 
 // input validation variables
 int input_character;
@@ -26,6 +31,9 @@ bool input_character_exit = false;
 bool exit_thread_started = false;
 bool typing_mode = false;
 unsigned char char_input_character;
+
+// UI elements
+
 
 // colors:
 const std::string color_reset  = "\033[0m";
@@ -35,12 +43,34 @@ const std::string color_yellow = "\033[33m";
 const std::string color_blue   = "\033[34m";
 const std::string color_cyan   = "\033[36m";
 
-// Parameter switches
+// Input Parameter switches
 bool arg_verbose = false;
 bool arg_help    = false;
 
+void usage () {
+    const char *usage_text = R""""(usage: video-client
+
+Arguments:
+    -h, --help                      Show this page
+    -v, --verbose, --debug          Show debug logs)"""";
+
+    std::cout << usage_text << "\n";
+}
+
+// Append file
+void append_file (std::string file, std::string content) {
+    std::fstream f;
+    f.open(file, std::ios::app);
+    if (!f)
+        std::cout << "File does not exist: " << file << "\n";
+    else {
+        f << content;
+        f.close();
+    }
+}
+
 // Log functions
-void log_main(std::string logmsg, int severity = 0, bool file = false) {
+void log_main ( std::string logmsg, int severity = 0, bool file = false ) {
     std::string severity_prefix;
     if ( severity == 0 ) {
         severity_prefix = "DEBUG  ";
@@ -65,15 +95,18 @@ void log_main(std::string logmsg, int severity = 0, bool file = false) {
         } else {
             std::cerr << fullmsg;
         }
-    } else {
+    } else if ( file == true ) {
         fullmsg = severity_prefix + " - " + logmsg + "\n";
-        std::cout << "ToFile: " << fullmsg;
+        append_file(logfile, fullmsg);
     }
 }
-void log(std::string message, int severity = 0) {
-    if (logging = true){
-        log_main(message, severity, logfile);
+void log ( std::string message, int severity = 0 ) {
+    if ( debug == false ){
+        if ( severity == 0 ) {
+            return;
+        }
     }
+    log_main(message, severity, log_to_file);
 }
 
 // Exit check thread, multiple keys return 27 including escape and arrow keys.
@@ -123,8 +156,7 @@ void THREAD_input () {
     new_tattr=old_tattr;
     new_tattr.c_lflag &=(~ICANON & ~ECHO);
     tcsetattr(STDIN_FILENO,TCSANOW,&new_tattr);
-    // Continous loop
-    while ( true ) {
+    while ( true ) { // Continous loop
         usleep(100); // 0.1MS
         if ( collapse_threads == true ) {
             break;
@@ -140,18 +172,69 @@ void THREAD_input () {
     tcsetattr(STDIN_FILENO,TCSANOW,&old_tattr);
 }
 
-int main() {
+void draw_ui ( int w, int h ) {
+    std::cout << "Width: " << w << " Height: " << h << "\n";
+}
 
-    std::thread input_thread(THREAD_input);
+int main ( int argc, char *argv[] ) {
+
+
+    // Parse arguments
+    int argument_iteration = 0;
+    if ( argc > 100 ) { std::cout << "Too many arguments!\n"; usage(); return 1; }
+    while ( true ) {
+        ++argument_iteration;
+        if ( argument_iteration >= argc ) {
+            break;
+        }
+        std::string argument_as_string( argv[argument_iteration] );
+        int argument_char_0_int = argument_as_string[0];
+        int argument_char_1_int = argument_as_string[1];
+        if ( argument_char_0_int == 45 ) {
+            if ( argument_char_1_int == 45 ) { // Double dash parameter, still WIP
+                std::cout << "Double Dash!\n";
+            } else { // Single dash parameter
+                for ( int argument_char_i = 1; argument_char_i < argument_as_string.length() ; ++argument_char_i ) {
+                    int argument_char_current = argument_as_string[argument_char_i];
+                    if ( argument_char_current == 118 ) { debug = true; log("Debugging Enabled!"); }
+                    else if ( argument_char_current == 104 ) { usage(); return 0; }
+                    else { std::cout << "Unknown parameter: -" << argument_as_string[argument_char_i] << "\n"; usage(); return 1; }
+                }
+            }
+        } else {
+            std::cout << "Unknown parameter: " << argv[argument_iteration] << "\n";
+            usage();
+            return 1;
+        }
+        //if ( argument_char_0 == "-" ) { std::cout << "Matching Single Dash!\n"; }
+        log("Success on parsing parameter: " + argument_as_string);
+    }
+
+    // Local UI Elements
+    int tmp_w, tmp_h; // Used to store previous window size to detect changes.
+    struct winsize size;
+
+    std::thread input_thread(THREAD_input); // Start input thread
 
     while ( true ) { // Main loop
 
         usleep(10000); // 10MS
+
         if ( collapse_threads == true ) {
             log("Exiting main function");
             break;
         }
+
+        ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+        if ( tmp_w != size.ws_col || tmp_h != size.ws_row ) {
+            tmp_w = size.ws_col;
+            tmp_h = size.ws_row;
+            draw_ui(size.ws_col, size.ws_row);
+        }
     }
+
+    log("testlog debug");
+    log("testlog error", 2);
 
     collapse_threads = true;
 
